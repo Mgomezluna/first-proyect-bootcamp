16f6c2040cecd09d238cd431cd706df99774dc98
Marianella Gomez Luna <mgomezluna@ar0fvfzw0kvl415.ml.com>
2021-09-10 15:21:32 -0300

    ya funcionando

--- index.js
+++ index.js
@@ -6,27 +6,16 @@ function Computadora(ram, cpu, disco, monitor, gpu){
  this.gpu = gpu;
 }
 
-const compu1 = new Computadora("2gb", "3gb", "450gb", "telcel", false);
-const compu2 = new Computadora("2gb", "3gb", "2050gb", "telcel", false);
-const compu3 = new Computadora("2gb", "3gb", "460gb", "telcel", true);
-const compu4 = new Computadora("2gb", "3gb", "4500gb", "telcel", false);
-const compu5 = new Computadora("2gb", "6gb", "4330gb", "telcel", true);
 
+compu = [];
+compu.push(new Computadora("2gb", "3gb", "450gb", "telcel", false));
+compu.push(new Computadora("2gb", "3gb", "2050gb", "telcel", false));
+compu.push(new Computadora("2gb", "3gb", "460gb", "telcel", true));
+compu.push(new Computadora("2gb", "3gb", "4500gb", "telcel", false));
+compu.push(new Computadora("2gb", "6gb", "4330gb", "telcel", true));
 
-const devolverLista = ( Computadora, compu1, compu2, compu3, compu4, compu5) =>{
+let computadorasConGpu = compu.filter(compu => compu.gpu)
 
-    Computadora.forEach(compu => {
-        compu.push(compu1);
-        compu.push(compu2);
-        compu.push(compu3);
-        compu.push(compu4);
-        compu.push(compu5);
-
-    });
-
-  
-    return Computadora.compu;
-}
-
-console.log(Computadora);
-
+console.log(compu.length);
+console.log(compu);
+console.log(computadorasConGpu);
