91a4b08c7c7b9fe09ea67eff6e7da405ce3db5bb
Marianella Gomez Luna <mgomezluna@ar0fvfzw0kvl415.ml.com>
2021-09-10 14:52:16 -0300

    arreglos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Computadora = (ram, cpu, disco, monitor, gpu) => {
+function Computadora(ram, cpu, disco, monitor, gpu){
  this.ram = ram;
  this.cpu = cpu;
  this.disco = disco;
@@ -6,19 +6,27 @@ const Computadora = (ram, cpu, disco, monitor, gpu) => {
  this.gpu = gpu;
 }
 
-const devolverLista = ( computadora ) =>{
-    let listaComputadoras = [];
-    computadora.forEach(c => {
+const compu1 = new Computadora("2gb", "3gb", "450gb", "telcel", false);
+const compu2 = new Computadora("2gb", "3gb", "2050gb", "telcel", false);
+const compu3 = new Computadora("2gb", "3gb", "460gb", "telcel", true);
+const compu4 = new Computadora("2gb", "3gb", "4500gb", "telcel", false);
+const compu5 = new Computadora("2gb", "6gb", "4330gb", "telcel", true);
 
-        listaComputadoras.push(
-            new computadora("2gb", "3gb", "450gb", "telcel", false),
-            new computadora("2gb", "3gb", "450gb", "telcel", false),
-            new computadora("2gb", "3gb", "450gb", "telcel", true),
-            new computadora("2gb", "3gb", "450gb", "telcel", false),
-            new computadora("2gb", "6gb", "4330gb", "telcel", true),
-        )
+
+const devolverLista = ( Computadora, compu1, compu2, compu3, compu4, compu5) =>{
+
+    Computadora.forEach(compu => {
+        compu.push(compu1);
+        compu.push(compu2);
+        compu.push(compu3);
+        compu.push(compu4);
+        compu.push(compu5);
 
     });
-    console.log(listaComputadoras);
-    return listaComputadoras;
+
+  
+    return Computadora.compu;
 }
+
+console.log(Computadora);
+
